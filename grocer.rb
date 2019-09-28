fb2c860d6020abec098f119fe2b85e757c131110
Stefanie Davis <davisstefanie2010@gmail.com>
2019-09-28 05:49:48 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -38,5 +38,6 @@ end
 def checkout(cart,coupons)
   hash_cart = consolidate_cart(array)
   applied_coupons = apply_coupons(hash_cart,coupons)
-  applied_discount = apply_clearance()
+  applied_discount = apply_clearance(applied_coupons)
+  
 end
