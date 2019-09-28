123a93fbd7944bd7cb199e3305055f0b3f1e52e6
Stefanie Davis <davisstefanie2010@gmail.com>
2019-09-28 06:00:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -39,8 +39,7 @@ def checkout(cart,coupons)
   hash_cart = consolidate_cart(cart)
   applied_coupons = apply_coupons(hash_cart,coupons)
   applied_discount = apply_clearance(applied_coupons)
-  total = applied_discount.reduce(0) {|acc, (key,value)| 
-    acc += value[:price]*value[:count]
+  total = applied_discount.reduce(0) {|acc, (key,value)| acc += value[:price]*value[:count]
     total > 100 ? total * 0.9 : total
   }
   
